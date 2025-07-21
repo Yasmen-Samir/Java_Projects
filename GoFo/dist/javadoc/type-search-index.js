1647b7118f6a02fc8e65eb6108162b071db01b58
yasmen samir <85004204+Yasmen-Samir@users.noreply.github.com>
2025-07-21 21:05:02 +0300

    Delete GoFo directory

--- GoFo/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"gofo","l":"Booking"},{"p":"gofo","l":"Ewallet"},{"p":"gofo","l":"GoFo"},{"p":"gofo","l":"Player"},{"p":"gofo","l":"Playground"},{"p":"gofo","l":"PlaygroundOwner"},{"p":"gofo","l":"Team"}];updateSearchResults();
